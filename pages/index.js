f8e5606a93f26e0c06fb7c9e0e0397367690d29a
nothikarulol <116022988+nothikarulol@users.noreply.github.com>
2022-11-01 09:31:29 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@
 <html>
     <head>
         <meta charset="utf-8">
+            
+            </meta>
 
         <title>Adams game shack</title>
 
@@ -13,12 +15,8 @@
         <h3>Welcome to my game website! This is the home tab so click on the game tab to get started!</h3>
         <h4>Version 1.0: New website adding games soon</h4>
 
-        
-        
-        
+       
     </body>
-
-   </meta>
     
 
 </html>
